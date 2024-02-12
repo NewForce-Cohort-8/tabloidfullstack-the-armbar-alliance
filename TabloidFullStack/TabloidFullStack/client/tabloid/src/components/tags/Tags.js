d9df416098d2870ebfa31c21b7f22328e2faccd6
Chelsea Hatton <chelseahamlin1992@gmail.com>
2024-02-12 13:31:42 -0500

    made basic changes

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/tags/Tags.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/tags/Tags.js
@@ -6,11 +6,6 @@ export const Tags = ({ tag }) => {
   return (
     <div className="m-4">
       <div>
-        {/* <p>
-          <Link to={`/tags/${tag.id}`}>
-          <strong>{tag.name}</strong>
-          </Link>
-        </p> */}
         <div>
           <p>{tag.name}</p>
         </div>
