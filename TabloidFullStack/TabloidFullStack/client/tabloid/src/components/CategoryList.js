e63324837bbe110e5e54e319005f1796197cb2a9
Kari Rehe <karinicole84@gmail.com>
2024-02-13 15:43:19 -0500

    working Delete

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/CategoryList.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/CategoryList.js
@@ -1,66 +1,8 @@
-// // CategoryList.js
-// import React, { useState, useEffect } from "react";
-// import { Category } from "./Category";
-// import { CategoryForm } from "./CategoryForm";
-// import { getAllCategories } from "../Managers/CategoryManager";
-// import { useNavigate } from "react-router-dom";
-
-// const CategoryList = () => {
-//   const [categories, setCategories] = useState([]);
-//   const navigate = useNavigate();
-
-//   const getCategories = () => {
-//     getAllCategories().then((allCategories) => setCategories(allCategories));
-//   };
-  
-
-//   const updateCategoriesState = () => {
-//     return getAllCategories().then((categoryArray) => {
-//       setCategories(categoryArray);
-//     });
-//   };
-
-//   const deleteCategory = (id) => {
-//     const confirmDelete = window.confirm("Are you sure you would like to delete this category?");
-//     if (confirmDelete) {
-//       deleteCategory(id).then(() => {getCategories();})
-//     }
-//   }
-
-//   useEffect(() => {
-//     updateCategoriesState();
-//   }, []);
-
-//   const handleEdit = (categoryId) => {
-//     navigate(`/categories/edit/${categoryId}`);
-//   };
-
-//   return (
-//     <div className="container">
-//       <div className="row justify-content-center">
-//         <div className="cards-column">
-//           <CategoryForm updateCategoriesState={updateCategoriesState} />
-//           {categories.map((category) => (
-//             <Category
-//               key={category.id}
-//               category={category}
-//               onEdit={handleEdit}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CategoryList;
-
-
 // CategoryList.js
 import React, { useState, useEffect } from "react";
 import { Category } from "./Category";
 import { CategoryForm } from "./CategoryForm";
-import { getAllCategories, deleteCategory as deleteCategoryAPI, deleteCategory } from "../Managers/CategoryManager"; // Import deleteCategory with an alias
+import { getAllCategories, deleteCategory } from "../Managers/CategoryManager"; 
 import { useNavigate } from "react-router-dom";
 
 const CategoryList = () => {
@@ -77,10 +19,11 @@ const CategoryList = () => {
     });
   };
 
-  const handleDelete = (id) => { // Rename the function to handleDelete to avoid naming conflict
+  const handleDelete = (id) => { 
     const confirmDelete = window.confirm("Are you sure you would like to delete this category?");
     if (confirmDelete) {
-      deleteCategory(id).then(() => { // Use the imported deleteCategory function
+        // if Bad Request window.alert this category has dependencies it would go here...
+      deleteCategory(id).then(() => { 
         getCategories();
       });
     }
